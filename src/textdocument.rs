046809b48965da8d2b81ad73d0a774e7617548d6
proegssilb <minifig404@gmail.com>
2020-09-01 22:05:25 -0500

    [minor(document)] Change text framework, add String insertion
    Switched test framework from property testing to built-in plus fake-rs. Required to actually debug tests.
    Implemented inserting Strings into the Document. This includes a String insertion splitting a line.

--- src/textdocument.rs
+++ src/textdocument.rs
@@ -92,17 +92,15 @@ impl PlainTextDocument {
     }
 
     fn insert_string(&mut self, cursor: Loc, insertion: String) {
-        let mut loc = cursor.clone();
-        for c in insertion.chars() {
-            self.content[loc.line].insert(loc.col, c);
-            loc.col += 1;
-            if "\r\n".contains(c) {
-                let (start, rest) = self.content[loc.line].split_at(loc.col);
-                let (start_str, rest_str) = (start.to_owned(), rest.to_owned());
-                self.content[loc.line] = start_str;
-                loc.line += 1;
-                loc.col = 0;
-                self.content.insert(loc.line, rest_str);
+        if cursor.col == 0 {
+            self.content.insert(cursor.line, insertion);
+        } else {
+            let extra = self.content[cursor.line].split_off(cursor.col);
+            self.content[cursor.line].push_str(&insertion);
+            if insertion.ends_with('\n') {
+                self.content.insert(cursor.line + 1, extra);
+            } else {
+                self.content[cursor.line].push_str(&extra);
             }
         }
     }
@@ -143,114 +141,130 @@ impl<'a> Document for &'a PlainTextDocument {
 #[cfg(test)]
 mod test {
     use super::*;
-    use proptest::prelude::*;
     use std::time::Duration;
-
-    /// A proptest strategy to return a raw text document and a location pointing
-    /// to a valid spot within the document.
-    fn text_and_loc() -> impl Strategy<Value = (Vec<String>, Loc)> {
-        prop::collection::vec(".+", 1..5)
-            .prop_flat_map(|doc| {
-                let lines = doc.len();
-                (Just(doc), 0..lines)
-            })
-            .prop_flat_map(|(doc, line)| {
-                let columns = doc[line].len();
-                (Just(doc), Just(line), 0..columns)
-            })
-            .prop_filter("Column must be a character boundary", |(doc, line, col)| doc[*line].is_char_boundary(*col))
-            .prop_map(|(doc, line, column)| {
-                (doc, Loc {line: line, col: column})
-            })
-    }
+    use fake::{Fake, Faker};
     
-    proptest! {
-        #![proptest_config(ProptestConfig {timeout: 600, ..ProptestConfig::default()})]
+    #[test]
+    fn cursor_getter_gets() {
+        let ln : usize = Faker.fake::<usize>();
+        let c : usize = Faker.fake::<usize>();
+        let mut doc = PlainTextDocument::new();
+        doc.cursor = Loc { line: ln, col: c };
 
-        // Document impl tests
-        #[test]
-        fn cursor_getter_gets(ln in 0usize..1000, c in 0usize..500) {
-            let mut doc = PlainTextDocument::new();
-            doc.cursor = Loc { line: ln, col: c };
+        let found_loc = doc.cursor_loc();
 
-            let found_loc = doc.cursor_loc();
+        assert_eq!(doc.cursor, found_loc);
+    }
 
-            prop_assert_eq!(doc.cursor, found_loc);
+    #[test]
+    fn lines_yields_document() {
+        let lns = Faker.fake::<Vec<String>>();
+        let mut doc = PlainTextDocument::new();
+        doc.content = lns.clone();
+
+        let found_lines : Vec<String> = doc.lines().collect();
+        assert_eq!(found_lines, lns);
+    }
+
+    #[test]
+    fn modified_getter_gets() {
+        let mut doc = PlainTextDocument::new();
+        assert_eq!(doc.is_modified(), false);
+
+        doc.modified = true;
+        assert_eq!(doc.is_modified(), true);
+
+        doc.modified = false;
+        assert_eq!(doc.is_modified(), false);
+    }
+
+    #[test]
+    fn event_sent_gets_received() {
+        let doc = PlainTextDocument::new();
+        let send_path = Faker.fake::<String>();
+        let ev = DocumentEdit::SaveAs(send_path.clone());
+
+        let res = doc.edits_channel().send(ev);
+        assert_eq!(res.is_ok(), true);
+
+        let received_res = doc.edits_receiver.recv_timeout(Duration::from_millis(50));
+        assert_eq!(received_res.is_ok(), true);
+
+        let received = received_res.unwrap_or(DocumentEdit::Close);
+
+        match received {
+            DocumentEdit::SaveAs(path) => assert_eq!(path, send_path),
+            _ => assert_eq!(1, 0), // This should be straight-up impossible...
         }
+    }
 
-        #[test]
-        fn lines_yields_document(lns in prop::collection::vec(".*", 0..1000) ) {
-            let mut doc = PlainTextDocument::new();
-            doc.content = lns.clone();
+    // Event handling tests
+    fn text_and_loc() -> (Vec<String>, Loc) {
+        let mut text = Faker.fake::<Vec<String>>();
+        // for ln in text.iter_mut() {
+        //     ln.push('\n');
+        // }
+        let l = (0..text.len()).fake::<usize>();
+        let char_locs : Vec<(usize, char)> = text[l].char_indices().collect();
+        let c = (0..text[l].chars().count()).fake::<usize>();
+        return (text, Loc {line: l, col: char_locs[c].0});
+    }
 
-            let found_lines : Vec<String> = doc.lines().collect();
-            prop_assert_eq!(found_lines, lns);
-        }
+    #[test]
+    fn close_event_closes_doc() {
+        let mut doc = PlainTextDocument::new();
+        let handled = doc.handle_event(DocumentEdit::Close);
+        assert_eq!(handled, false);
+    }
 
-        #[test]
-        fn modified_getter_gets(flag in any::<bool>()) {
-            let mut doc = PlainTextDocument::new();
-            doc.modified = flag;
+    #[test]
+    fn insert_char_replaces_removed_char() {
+        let (text, loc) = text_and_loc();
 
-            prop_assert_eq!(doc.is_modified(), flag);
-        }
+        let mut starting_text = text.clone();
+        let char_removed = (*starting_text)[loc.line].remove(loc.col);
+        let mut doc = PlainTextDocument::new();
+        doc.content = starting_text;
 
-        #[test]
-        fn event_sent_gets_received(send_path in any::<String>()) {
-            let doc = PlainTextDocument::new();
-            let ev = DocumentEdit::SaveAs(send_path.clone());
+        let handled = doc.handle_event(DocumentEdit::InsertChar(loc, char_removed));
 
-            doc.edits_channel().send(ev)?;
+        assert_eq!(text, doc.content);
+        assert_eq!(handled, true);
+    }
 
-            let received = doc.edits_receiver.recv_timeout(Duration::from_millis(50))?;
+    #[test]
+    fn insert_str_can_insert_line() {
+        let (text, loc) = text_and_loc();
 
-            match received {
-                DocumentEdit::SaveAs(path) => prop_assert_eq!(path, send_path),
-                _ => prop_assert_eq!(1, 0), // This should be straight-up impossible...
-            }
-        }
+        let mut starting_text = text.clone();
+        let line_removed : String = starting_text.remove(loc.line);
+        let mut doc = PlainTextDocument::new();
+        doc.content = starting_text;
+        let test_loc = Loc {line: loc.line, col: 0};
 
-        // Event handling tests
-        #[test]
-        fn close_event_closes_doc(modified in any::<bool>()) {
-            let mut doc = PlainTextDocument::new();
-            doc.modified = modified;
-            doc.edits_channel().send(DocumentEdit::Close)?;
+        let handled = doc.handle_event(DocumentEdit::InsertStr(test_loc, line_removed));
 
-            // This should return successfully immediately
-            // If close event isn't handled right, it could run forever
-            doc.thread_method()?;
-        }
+        assert_eq!(text, doc.content);
+        assert_eq!(handled, true);
+    }
 
-        #[test]
-        fn insert_char_replaces_removed_char((text, loc) in text_and_loc()) {
-            // This test might be unreliable
-            let mut starting_text = text.clone();
-            let char_removed = (*starting_text)[loc.line].remove(loc.col);
-            let mut doc = PlainTextDocument::new();
-            doc.content = starting_text;
+    #[test]
+    fn insert_newline_splits_line() {
+        let (text, loc) = text_and_loc();
+        let inserted = Faker.fake::<String>() + "\n";
+        let mut doc = PlainTextDocument::new();
+        doc.content = text.clone();
 
-            doc.handle_event(DocumentEdit::InsertChar(loc, char_removed));
+        let handled = doc.handle_event(DocumentEdit::InsertStr(loc, inserted.clone()));
 
-            prop_assert_eq!(text, doc.content);
-        }
+        let mut expected_text = text.clone();
+        let split_line = expected_text[loc.line].split_at(loc.col);
+        let first = split_line.0.to_string();
+        let second = split_line.1.to_string();
+        expected_text[loc.line] = first + &inserted;
+        expected_text.insert(loc.line + 1, second);
 
-        #[test]
-        fn insert_newline_splits_line((text, loc) in text_and_loc()) {
-            prop_assert_eq!(1, 1);
-        }
-
-        #[test]
-        fn insert_str_can_insert_line((text, loc) in text_and_loc()) {
-            let mut starting_text = text.clone();
-            let line_removed : String = starting_text.remove(loc.line);
-            let mut doc = PlainTextDocument::new();
-            doc.content = starting_text;
-            let test_loc = Loc {line: loc.line, col: 0};
-
-            doc.handle_event(DocumentEdit::InsertStr(test_loc, line_removed + "\n"));
-
-            prop_assert_eq!(text, doc.content);
-        }
+        assert_eq!(expected_text, doc.content);
+        assert_eq!(handled, true);
     }
 }
