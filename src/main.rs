cd3ebe58bdf8cff75559f49f503835c5e5a9e712
proegssilb <proegssilb@gmail.com>
2025-04-20 21:35:15 -0400

    Remove non-functioning devcontainer, add more menu items.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use std::process::exit;
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+
+    ui.on_file_exit(|| {
+       exit(0); 
+    });
+    
+    ui.on_menu_click(|cmd| {
+       println!("{:?}", cmd); 
+    });
 
     ui.run()
 }
